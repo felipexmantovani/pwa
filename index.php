1ea0f9a5db27884c72a04489cb9813349256ca88
Felipe Mantovani <felipexmantovani@gmail.com>
2018-10-27 09:58:55 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PWA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" media="screen" href="css/materialize.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+</head>
+<body>
+    <nav class="nav-extended">
+        <div class="nav-wrapper">
+            <a href="index.php" class="brand-logo"><img src="images/logo.png" alt=""></a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+            <ul id="menu" class="right hide-on-med-and-down">
+
+            </ul>
+        </div>
+    </nav>
+
+    <ul id="mobile-demo" class="sidenav">
+    </ul>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/materialize.min.js"></script>
+</body>
+</html>
